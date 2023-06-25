88b66e3a57212c90e3d3fd3c191aa2fe08e8555e
Ricardo Gomes <pb.ricardogomes@gmail.com>
2023-06-25 15:42:06 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/com/purplemonkeys/getx_intro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.purplemonkeys.getx_intro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'value_controller.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final textController = TextEditingController();
+
+  final valueController = ValueController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Valor
+            GetBuilder<ValueController>(
+              init: valueController,
+              builder: (ctrl) {
+                return Text('Valor definido: ${ctrl.definedValue}');
+              },
+            ),
+            //Campo
+            TextField(
+              controller: textController,
+            ),
+            //Botão
+            GetBuilder<ValueController>(
+              init: valueController,
+              builder: (ctrl) {
+                return ctrl.isLoading ? const CircularProgressIndicator() : ElevatedButton(
+                  onPressed: () {
+                    String value = textController.text;
+
+                    valueController.setValue(value);
+                  },
+                  child: const Text('Confirmar'),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
